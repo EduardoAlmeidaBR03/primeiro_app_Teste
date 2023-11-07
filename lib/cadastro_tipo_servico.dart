1c093b76daa209044dadea7ec691aa66aabcaadf
Welington Gulinelli Costa <welingtongulinelli69@gmail.com>
2023-11-07 07:28:09 -0300

    07/11

--- lib/cadastro_tipo_servico.dart
+++ lib/cadastro_tipo_servico.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 class TipoServico {
   String nome;
+  String descricao; // Adicione a descrição do tipo de serviço
   double valor;
-  TipoServico({required this.nome, required this.valor});
+
+  TipoServico({required this.nome, required this.descricao, required this.valor});
 }
 
 class CadastroTipoServico extends StatefulWidget {
@@ -13,21 +15,24 @@ class CadastroTipoServico extends StatefulWidget {
 
 class _CadastroTipoServicoState extends State<CadastroTipoServico> {
   TextEditingController nomeController = TextEditingController();
+  TextEditingController descricaoController = TextEditingController(); // Controlador para a descrição
   TextEditingController valorController = TextEditingController();
 
   List<TipoServico> tiposServico = [];
 
   void cadastrarTipoServico() {
     String nome = nomeController.text;
+    String descricao = descricaoController.text; // Obtenha a descrição
     double valor = double.parse(valorController.text);
 
-    TipoServico novoTipoServico = TipoServico(nome: nome, valor: valor);
+    TipoServico novoTipoServico = TipoServico(nome: nome, descricao: descricao, valor: valor);
 
     setState(() {
       tiposServico.add(novoTipoServico);
     });
 
     nomeController.clear();
+    descricaoController.clear(); // Limpe o campo de descrição
     valorController.clear();
   }
 
@@ -38,7 +43,7 @@ class _CadastroTipoServicoState extends State<CadastroTipoServico> {
         return AlertDialog(
           title: Text('Tipos de Serviço Cadastrados'),
           content: Column(
-            children: tiposServico.map((tipoServico) => Text('${tipoServico.nome} - R\$ ${tipoServico.valor.toStringAsFixed(2)}')).toList(),
+            children: tiposServico.map((tipoServico) => Text('${tipoServico.nome} - ${tipoServico.descricao} - R\$ ${tipoServico.valor.toStringAsFixed(2)}')).toList(),
           ),
           actions: [
             ElevatedButton(
@@ -67,6 +72,12 @@ class _CadastroTipoServicoState extends State<CadastroTipoServico> {
               controller: nomeController,
               decoration: InputDecoration(
                 labelText: 'Nome',
+              ),
+            ),
+            TextField(
+              controller: descricaoController,
+              decoration: InputDecoration(
+                labelText: 'Descrição', // Campo de descrição
               ),
             ),
             TextField(
